6cf83d0f9262e0e7bb4b9627616d909ee8d5f5e8
melipescu <popescumelinda76@gmail.com>
2025-11-14 07:19:49 +0200

    setting up mongoose in progress

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/test');
+}
+
+// *another way of connecting to mongodb
+// mongoose.connect('mongodb://localhost:27017/test')
+//     .then(() => {
+//         console.log("CONNECTION OPEN!!!")
+//     })
+//     .catch(err => {
+//         console.log("OH NO, ERROR!!!")
+//         console.log(err)
+//     })
+
+const carSchema = new mongoose.Schema({
+    numarInmatriculare: String,//date - is string ok?, is property name ok as camelCase?
+    sasiu: Number,//numbers and letters
+    leasingFile: Number,
+    leasingStart: String,
+    leasingFinish: String,
+    rca: String,
+    casco: String,
+    itp: String,
+    rovinieta: String,
+
+});
 
 const app = express();
 
